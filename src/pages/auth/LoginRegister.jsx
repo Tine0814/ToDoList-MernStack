a1c5ac30ee59ef15760d3aeeadeabc7ccd978e0f
unknown <djhaybernardo@gmail.com>
2023-06-21 16:05:29 +0800

    second commit

--- src/pages/auth/LoginRegister.jsx
+++ src/pages/auth/LoginRegister.jsx
@@ -4,7 +4,9 @@ const LoginRegister = () => {
   return (
     <>
       <div className="w-full h-screen grid place-items-center p-10">
-        <div className="bg-red-200 h-[500px] w-[1000px]"></div>
+        <div className="relative bg-red-200 h-[500px] w-[1000px]">
+          <div className="absolute bg-red-400 h-[500px] w-[50%] right-0"></div>
+        </div>
       </div>
     </>
   );
