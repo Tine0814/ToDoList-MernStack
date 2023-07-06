6c149cdd980ca0a72f2f89fbed234b1f9d8db087
DSTN <djhaybernardo@gmail.com>
2023-07-06 16:42:59 +0800

    add dispatch to update the list to be null

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -1,7 +1,9 @@
 import { useUserContext } from "./useUserContext";
+import { useListsContext } from "../hooks/useListsContext";
 
 export const useLogout = () => {
   const { dispatch } = useUserContext();
+  const { dispatch: listDispatch } = useListsContext();
 
   const logout = () => {
     //remove user From storage
@@ -9,6 +11,7 @@ export const useLogout = () => {
 
     // dispatch logout Action
     dispatch({ type: "LOGOUT_USER" });
+    listDispatch({ type: "SET_LISTS", payload: null });
   };
 
   return { logout };
